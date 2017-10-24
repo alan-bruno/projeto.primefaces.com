1053067e27f0911e6be77b3e5944bd70f9bb2261
Alan Bruno <alanbruno.rj@gmail.com>
2017-10-24 10:42:23 -0200

    Primeiro Commit

---
+++ src/projeto/primefaces/com/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,7 @@
+package projeto.primefaces.com.implementacao.crud;
+
+public class VariavelConexaoUtil {
+	
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdvc/datasource";
+
+}
